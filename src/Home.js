96d9ba1b1899ddf4669ee71d6dc97e7460bdd3c2
KAMATCHI154 <202100958@sigc.edu>
2024-03-09 04:19:48 -0800

    first commit

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="home-container">
       <h3>Welcome to React-App</h3>
-      <img className="home-img" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.ag-grid.com%2Fbuilding-crud-in-ag-grid-with-graphql-and-react%2F&psig=AOvVaw0WLdHxwp7Vexnnk7EJ1anb&ust=1710069635982000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOCyqPuH54QDFQAAAAAdAAAAABAK" alt="Welcome picture" />
+      <img className="home-img" src="https://wallpapers.com/images/hd/high-resolution-fall-1920-x-1200-background-1qlfbqqs2kyn4kvg.jpg_" alt="Welcome picture" />
     </div>
   )
 }
