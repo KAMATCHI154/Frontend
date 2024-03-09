0ef114f2066aa7480323b957b6dd5445633432b2
KAMATCHI154 <202100958@sigc.edu>
2024-03-09 03:45:49 -0800

    first commit

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h3>Welcome to React-App</h3>
+      <img className="home-img" src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fblog.ag-grid.com%2Fbuilding-crud-in-ag-grid-with-graphql-and-react%2F&psig=AOvVaw0WLdHxwp7Vexnnk7EJ1anb&ust=1710069635982000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCOCyqPuH54QDFQAAAAAdAAAAABAK" alt="Welcome picture" />
+    </div>
+  )
+}
